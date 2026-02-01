883dd66a89ccccecc06f318158fac16d7cf3f2c1
Albarico <khenalbarico05@gmail.com>
2026-02-01 09:55:30 +0800

    Genesis

---
+++ Learnitallandroid1/Learnitallandroid1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Learnitallandroid1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Learnitallandroid1/Learnitallandroid1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Learnitallandroid1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
