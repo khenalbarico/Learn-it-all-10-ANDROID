d3b5adc65c28f7a8fbd7876069fbb8e781a7ba03
Albarico <khenalbarico05@gmail.com>
2026-03-07 15:07:39 +0800

    revert mauiprog1

--- Learnitallandroid1/Learnitallandroid1/MauiProgram.cs
+++ Learnitallandroid1/Learnitallandroid1/MauiProgram.cs
@@ -1,69 +1,26 @@
-﻿using Firebase.Auth;
-using Firebase.Auth.Providers;
-using Firebase.Auth.Repository;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Models1.Config;
+﻿using Microsoft.Extensions.Logging;
 
-namespace Learnitallandroid1;
-
-public static class MauiProgram
+namespace Learnitallandroid1
 {
-    public static MauiApp CreateMauiApp()
+    public static class MauiProgram
     {
-        var builder = MauiApp.CreateBuilder();
-
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
-
-        var appConfig = LoadJsonFromAppPackage("appsettings.json");
-        builder.Configuration.AddConfiguration(appConfig);
-
-        var mapped = builder.Configuration
-            .GetSection("FirebaseAuthentication")
-            .Get<AuthenticationConfig>()
-            ?? new AuthenticationConfig();
-
-        builder.Services.AddSingleton(mapped);
-
-        builder.Services.AddSingleton(sp =>
+        public static MauiApp CreateMauiApp()
         {
-            var mappedConfig = sp.GetRequiredService<AuthenticationConfig>();
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-            var firebaseClientConfig = new FirebaseAuthConfig
-            {
-                ApiKey = mappedConfig.ApiKey,
-                AuthDomain = mappedConfig.AuthDomain,
-                Providers =
-                [
-                    new EmailProvider()
-                ],
-                UserRepository = new FileUserRepository("firebase_auth_cache")
-            };
-
-            return new global::Firebase.Auth.FirebaseAuthClient(firebaseClientConfig);
-        });
 
 #if DEBUG
-        builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
-        return builder.Build();
-    }
-
-    private static IConfiguration LoadJsonFromAppPackage(string fileName)
-    {
-        using var stream = FileSystem.OpenAppPackageFileAsync(fileName)
-            .GetAwaiter()
-            .GetResult();
-
-        return new ConfigurationBuilder()
-            .AddJsonStream(stream)
-            .Build();
+            return builder.Build();
+        }
     }
 }
