63f2a5045fd93701153331433d98e1460b0b3755
Albarico <khenalbarico05@gmail.com>
2026-03-07 14:42:11 +0800

    merge1

---
+++ Learnitallandroid1/Models1/Authentication/SendOobCodeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Models1.Authentication;
+
+public sealed class SendOobCodeRequest
+{
+    public string RequestType { get; set; } = "VERIFY_EMAIL";
+    public string IdToken { get; set; } = string.Empty;
+}
---
+++ Learnitallandroid1/Models1/Config/AuthenticationConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Models1.Config;
+
+public class AuthenticationConfig
+{
+    public string ApiKey { get; set; } = string.Empty;
+    public string AuthDomain {  get; set; } = string.Empty;
+}
